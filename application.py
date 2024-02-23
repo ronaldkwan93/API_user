e0170eecda48074051904042bb032d52e04fe13a
RonaldKwann <14719@coderacademy.edu.au>
2024-02-24 00:51:13 +1100

    /users route

--- application.py
+++ application.py
@@ -1,14 +1,32 @@
 from flask import Flask
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.password}"
 
 @app.route('/')
 def index():
     return 'Hello!'
 
 @app.route('/users')
-def get_drinks():
-    return {"users" : "user_data"}
+def get_users():
+    users = User.query.all()
+
+    output = []
+    for user in users:
+        user_data = {'name': user.name, 'password': user.password}
+
+        output.append(user_data)
+    return {"users" : output}
 
